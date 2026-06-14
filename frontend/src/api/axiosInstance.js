a9d78690f493aa4f3383581d6e6e495a1d46ff40
Ayanaw Demeke <ayanawdemeke@gmail.com>
2026-06-14 14:14:58 +0300

    deploy: update base URL to Render

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,8 +1,14 @@
 import axios from 'axios'
 
+//Locally
+//const api = axios.create({
+//  baseURL: 'http://localhost:8080/api',
+//  headers: { 'Content-Type': 'application/json' },
+//})
+
+//Remote server
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://kabarent.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 })
-
 export default api
